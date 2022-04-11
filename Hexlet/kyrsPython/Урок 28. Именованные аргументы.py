c4fd20076f68892bced9c54d96cec64c1f825a25
wermass <real432@rambler.ru>
2022-04-12 01:31:26 +0300

    тут не понимаю, почему на пустую строку выдает ошибку,в файле описание небольшое

--- Hexlet/kyrsPython/Урок 28. Именованные аргументы.py
+++ Hexlet/kyrsPython/Урок 28. Именованные аргументы.py
@@ -1,9 +1,13 @@
 def capitalize(name):
-  nam = f'{name[0].upper()}{name[1:]}'
-  print(nam)
-  return nam
+  name = f'{name[0].upper()}{name[1:]}'
+  while len(name) < 1: # пробовал if, одна и таже фигня, выдает invalid syntax при пустой строке, без пустой строки работает
+    print('')
+    break
+  return name
 
-capitalize('arya')
+print(capitalize('arya'))
+print(capitalize('')
+print('Arya')
 
 # Реализуйте функцию capitalize(), которая принимает непустую строку и приводит первую букву первого слова к верхнему регистру:
 
