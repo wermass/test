b6bb2210e597e59dc23a5df35b54b1575b4cf64f
wermass <real432@rambler.ru>
2022-04-18 10:58:01 +0300

    первая работа после др=)

---
+++ Hexlet/kyrsPython/Урок 40 Модули поглубже.py
@@ -0,0 +1,26 @@
+
+from kit import show_language, say_hello, say_bye
+def print_kit():
+  show_language()
+  say_hello()
+  say_bye()
+
+print_kit()
+
+                                # Задание
+# Директория src в файловом дереве содержит модуль kit.py. Изучите его.
+
+# Реализуйте функцию print_kit() и вызовите в ней все функции, которые определены в модуле kit.py. Предварительно импортируйте их.
+
+#                               fale:  kit.py
+
+# def show_language():
+#     print("Python")
+
+
+# def say_hello():
+#     print("Hello!")
+
+
+# def say_bye():
+#     print("Bye...")
